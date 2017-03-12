5b388581168a6187ce494da0c25e9b7eaff16cd0
Matt Davis <matthewpeterdavis@gmail.com>
2017-03-12 14:26:49 +0200

    make handler

--- src/server.js
+++ src/server.js
@@ -1,7 +1,15 @@
 var http = require('http');
 
-var server = http.createServer();
+var server = http.createServer(handler);
 
 server.listen(3000, function(){
     console.log("Server listening on port 3000. Ready to accept requests!");
 })
+
+var message = "We are making servers!"
+
+function handler (request, response){
+    response.writeHead(200,{"content-type":"text/html"});
+    response.write(message);
+    response.end();
+}
